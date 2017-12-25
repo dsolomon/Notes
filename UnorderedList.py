3e6b8e6deac27cfb56b243862a24634e7b649b99
dsolomon <david.solomon@colorado.edu>
2017-12-25 14:28:32 -0700

    problem 3-2 and notes

---
+++ UnorderedList.py
@@ -0,0 +1,29 @@
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item)
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
---
+++ linkedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.getData
+
+    def getNext(self):
+        return self.getNext
+
+    def selfData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
